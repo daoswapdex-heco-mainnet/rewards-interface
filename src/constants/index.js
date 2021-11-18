6d569d37a0286725eecf4172956841fbea85d91d
daoswap002 <daoswap@daoswap.cc>
2021-11-18 12:37:58 +0800

    alert address

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,8 +7,8 @@ export const CommunityRewardsDAOApproveContractAddress =
 export const CommunityRewardsDATContractAddress =
   "0xf8F08f8712A5c0ed32fB0774e48e97e1081dB7b2";
 
-export const DAOAddress = "0xd2f169c79553654452a3889b210AEeF494eB2374";
-export const DATAddress = "0x36653A4089DEB09A4782bf9EaEE5C8f4381ad010";
+export const DAOAddress = "0xc096332CAacF00319703558988aD03eC6586e704";
+export const DATAddress = "0x738B815eaDD06E0041b52B0C9d4F0d0D277B24bA";
 export const ZeroAddress = "0x0000000000000000000000000000000000000000";
 
 export const CHAIN_ID = process.env.VUE_APP_CHAIN_ID || 128;
